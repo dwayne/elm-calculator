76e126e73dbcb379e6a2c81a18f2eb2bb86ce205
Dwayne Crooks <me@dwaynecrooks.com>
2022-09-05 14:20:53 -0400

    Extract viewCalculator from main into Calculator as view

--- src/Calculator.elm
+++ src/Calculator.elm
@@ -5,11 +5,15 @@ module Calculator exposing
 
   , Display
   , toDisplay
+
+  , view
   )
 
 
 import Digit
 import Evaluator as E
+import Html as H
+import Html.Attributes as HA
 import Key exposing (Key(..))
 import Operator exposing (Operator)
 import Rational exposing (Rational)
@@ -193,6 +197,9 @@ toRational n =
         |> Maybe.withDefault Rational.zero
 
 
+-- DISPLAY
+
+
 type alias Display =
   { line1 : String
   , line2 : String
@@ -277,3 +284,220 @@ toPaddedDecimalString n =
 toExpr : List Token -> String
 toExpr =
   String.join "" << List.map Token.toString << List.reverse
+
+
+-- VIEW
+
+
+view : (Key -> msg) -> Calculator -> H.Html msg
+view onClick calculator =
+  H.div [ HA.class "calculator" ]
+    [ H.div [ HA.class "calculator__display" ]
+        [ viewDisplay <| toDisplay calculator
+        ]
+    , H.div [ HA.class "calculator__pad" ]
+        [ viewPad onClick
+        ]
+    ]
+
+
+viewDisplay : Display -> H.Html msg
+viewDisplay { line1, line2 } =
+  H.div [ HA.class "display" ]
+    [ H.div [ HA.class "display__line1" ] [ H.text line1 ]
+    , H.div [ HA.class "display__line2" ] [ H.text line2 ]
+    ]
+
+
+viewPad : (Key -> msg) -> H.Html msg
+viewPad onClick =
+  H.div [ HA.class "pad" ]
+    [ viewPadSlot
+        { position = (0, 0)
+        , span = Just Colspan
+        , keyOptions =
+            { style = Key.Primary
+            , onClick = onClick
+            }
+        , key = AC
+        }
+    , viewPadSlot
+        { position = (0, 2)
+        , span = Nothing
+        , keyOptions =
+            { style = Key.Default
+            , onClick = onClick
+            }
+        , key = Operator Operator.Div
+        }
+    , viewPadSlot
+        { position = (0, 3)
+        , span = Nothing
+        , keyOptions =
+            { style = Key.Default
+            , onClick = onClick
+            }
+        , key = Operator Operator.Mul
+        }
+    , viewPadSlot
+        { position = (1, 0)
+        , span = Nothing
+        , keyOptions =
+            { style = Key.Default
+            , onClick = onClick
+            }
+        , key = Digit Digit.Seven
+        }
+    , viewPadSlot
+        { position = (1, 1)
+        , span = Nothing
+        , keyOptions =
+            { style = Key.Default
+            , onClick = onClick
+            }
+        , key = Digit Digit.Eight
+        }
+    , viewPadSlot
+        { position = (1, 2)
+        , span = Nothing
+        , keyOptions =
+            { style = Key.Default
+            , onClick = onClick
+            }
+        , key = Digit Digit.Nine
+        }
+    , viewPadSlot
+        { position = (1, 3)
+        , span = Nothing
+        , keyOptions =
+            { style = Key.Default
+            , onClick = onClick
+            }
+        , key = Operator Operator.Sub
+        }
+    , viewPadSlot
+        { position = (2, 0)
+        , span = Nothing
+        , keyOptions =
+            { style = Key.Default
+            , onClick = onClick
+            }
+        , key = Digit Digit.Four
+        }
+    , viewPadSlot
+        { position = (2, 1)
+        , span = Nothing
+        , keyOptions =
+            { style = Key.Default
+            , onClick = onClick
+            }
+        , key = Digit Digit.Five
+        }
+    , viewPadSlot
+        { position = (2, 2)
+        , span = Nothing
+        , keyOptions =
+            { style = Key.Default
+            , onClick = onClick
+            }
+        , key = Digit Digit.Six
+        }
+    , viewPadSlot
+        { position = (2, 3)
+        , span = Nothing
+        , keyOptions =
+            { style = Key.Default
+            , onClick = onClick
+            }
+        , key = Operator Operator.Add
+        }
+    , viewPadSlot
+        { position = (3, 0)
+        , span = Nothing
+        , keyOptions =
+            { style = Key.Default
+            , onClick = onClick
+            }
+        , key = Digit Digit.One
+        }
+    , viewPadSlot
+        { position = (3, 1)
+        , span = Nothing
+        , keyOptions =
+            { style = Key.Default
+            , onClick = onClick
+            }
+        , key = Digit Digit.Two
+        }
+    , viewPadSlot
+        { position = (3, 2)
+        , span = Nothing
+        , keyOptions =
+            { style = Key.Default
+            , onClick = onClick
+            }
+        , key = Digit Digit.Three
+        }
+    , viewPadSlot
+        { position = (3, 3)
+        , span = Just Rowspan
+        , keyOptions =
+            { style = Key.Secondary
+            , onClick = onClick
+            }
+        , key = Equal
+        }
+    , viewPadSlot
+        { position = (4, 0)
+        , span = Just Colspan
+        , keyOptions =
+            { style = Key.Default
+            , onClick = onClick
+            }
+        , key = Digit Digit.Zero
+        }
+    , viewPadSlot
+        { position = (4, 2)
+        , span = Nothing
+        , keyOptions =
+            { style = Key.Default
+            , onClick = onClick
+            }
+        , key = Dot
+        }
+    ]
+
+
+type Span
+  = Colspan
+  | Rowspan
+
+
+viewPadSlot :
+  { position : (Int, Int)
+  , span : Maybe Span
+  , keyOptions : Key.Options msg
+  , key : Key
+  }
+  -> H.Html msg
+viewPadSlot { position, span, keyOptions, key } =
+  let
+    (r, c) =
+      position
+  in
+  H.div
+    [ HA.class "pad__slot"
+    , HA.class <| "r" ++ String.fromInt r
+    , HA.class <| "c" ++ String.fromInt c
+    , HA.class <|
+        case span of
+          Nothing ->
+            ""
+
+          Just Colspan ->
+            "colspan2"
+
+          Just Rowspan ->
+            "rowspan2"
+    ]
+    [ Key.view keyOptions key ]
